42fde1c5a0e5287b86627d09cfa2722a740bd19a
MJ-NJ <mjha_be21@thapar.edu>
2024-01-24 11:15:53 +0530

    LabComplete

--- Lab_1/Source.cpp
+++ Lab_1/Source.cpp
@@ -8,16 +8,80 @@ void init() {
 	glutCreateWindow("Simple Window");
 }
 
-void display() {
+// Empty Display
+void displayWindow() {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	gluOrtho2D(0.0, 100.0, 0.0, 100.0);
 }
 
-int main(int argc, char* argv[]) {
+// POINT
+void displayPoint() {
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPointSize(10.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+
+	glBegin(GL_POINTS);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glEnd();
+
+	glFlush();
+}
+
+// Line
+void displayLine() {
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPointSize(10.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(0.1f, 0.1f, 0.0f);
+	glVertex3f(0.5f, 0.5f, 0.0f);
+	glEnd();
+	glFlush();
+}
+
+// Rectangle
+void displayRectangle() {
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPointSize(10.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+
+	glBegin(GL_QUADS);
+	glVertex3f(-0.5f, 0.5f, 0.0f);
+	glVertex3f(0.5f, 0.5f, 0.0f);
+	glVertex3f(0.5f, -0.5f, 0.0f);
+	glVertex3f(-0.5f, -0.5f, 0.0f);
+	glEnd();
+
+	glFlush();
+}
+
+//TRIANGLE
+void displayTriangle() {
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glEnd();
+
+	glFlush();
+}
+int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	init();
-	glutDisplayFunc(display);
+	glutDisplayFunc(displayTriangle);
 	glutMainLoop();
 	return 0;
 }
